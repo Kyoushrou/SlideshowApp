26586d508f8a42730fbdbf21db13d4a9118b2981
kyousiro.emi <kikigigi05120214@outlook.jp>
2020-11-20 23:31:26 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 恵美　響詩郎 on 2020/11/20.
+//  Copyright © 2020 kyoushirou.emi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
